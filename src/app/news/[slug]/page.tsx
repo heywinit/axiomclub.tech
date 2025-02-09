db730f1537775e386900ef0857f3265ccaf70312
heywinit <heywinit@gmail.com>
2025-02-10 02:12:48 +0530

    fix compiler issues temporarily

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,17 +1,31 @@
-import { getStory } from "./utils";
-import { ClientArticle, MatrixOverlay } from "./components";
+// import { getStory } from "./utils";
+// import { ClientArticle, MatrixOverlay } from "./components";
 
-export default async function ArticlePage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const story = await getStory(params.slug);
+// type Props = {
+//   params: { slug: string };
+//   searchParams: { [key: string]: string | string[] | undefined };
+// };
 
-  return (
-    <main className="min-h-screen relative bg-black py-16 px-4">
-      <MatrixOverlay />
-      <ClientArticle story={story} slug={params.slug} />
-    </main>
-  );
+// export default async function ArticlePage({ params }: Props) {
+//   const story = await getStory(params.slug);
+
+//   return (
+//     <main className="min-h-screen relative bg-black py-16 px-4">
+//       <MatrixOverlay />
+//       <ClientArticle story={story} slug={params.slug} />
+//     </main>
+//   );
+// }
+
+// // Generate static params for all possible slugs
+// export async function generateStaticParams() {
+//   // You can fetch all possible slugs here
+//   // For now, returning an empty array as placeholder
+//   return [];
+// }
+
+import React from "react";
+
+export default function page() {
+  return <div>page</div>;
 }
