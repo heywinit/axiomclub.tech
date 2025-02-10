80ca2be5c0a3e1bd2a2c0fb15f9e96f122b68d2e
heywinit <heywinit@gmail.com>
2025-02-10 09:04:27 +0530

    team rearrangement

--- src/services/teamService.ts
+++ src/services/teamService.ts
@@ -15,28 +15,27 @@ export interface Members {
 export const teamService = {
   getTeam: (): Members[] => [
     {
-      name: "Vinesh Rajpurohit",
-      role: "Founder & Leader",
-      image: "/team/vinesh.jpg",
-      bio: "Usually writing code for screens, sometimes it also makes stuff fly",
-      skills: ["Full Stack", "Avionics" , "competitive programming"],
-      links: {
-        github: "https://github.com/heywinit",
-        linkedin: "https://www.linkedin.com/in/vinesh-rajpurohit/",
-        email: "vinesh@axiomclub.tech"
-      },
-    },
-
-    {
       name: "Vaidehi Shah",
       role: "Frontend Lead",
       image: "/team/vaidehi.jpg",
       bio: "Cooking, but not in the kitchen tho.",
-      skills: ["UI/UX", "Frontend" , "3D-Animation"],
+      skills: ["UI/UX", "Frontend", "3D-Animation"],
       links: {
         github: "https://github.com/VaidehiCodes",
         linkedin: "https://www.linkedin.com/in/vaidehi-shah21/",
-        email: "vaidehi@axiomclub.tech"
+        email: "vaidehi@axiomclub.tech",
+      },
+    },
+    {
+      name: "Vinesh Rajpurohit",
+      role: "Founder & Leader",
+      image: "/team/vinesh.jpg",
+      bio: "Usually writing code for screens, sometimes it also makes stuff fly",
+      skills: ["Full Stack", "Avionics", "competitive programming"],
+      links: {
+        github: "https://github.com/heywinit",
+        linkedin: "https://www.linkedin.com/in/vinesh-rajpurohit/",
+        email: "vinesh@axiomclub.tech",
       },
     },
 
@@ -45,11 +44,11 @@ export const teamService = {
       role: "Backend Lead",
       image: "/team/deepraj.jpg",
       bio: "I do not read terms and services",
-      skills: ["Backend" , "Architecture" , "AI/ML"],
+      skills: ["Backend", "Architecture", "AI/ML"],
       links: {
         github: "https://github.com/byedeep",
         linkedin: "https://www.linkedin.com/in/deepraj-bhati-0210a4330/",
-        email: "deep@axiomclub.tech"
+        email: "deep@axiomclub.tech",
       },
     },
   ],
